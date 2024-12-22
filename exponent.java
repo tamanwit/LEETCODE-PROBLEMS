9001d4b6e211f7ae1a6c004808dc3a1cdead5cc2
Tamanwit <141413502+tamanwit@users.noreply.github.com>
2024-12-22 16:24:04 +0530

    Create exponent.java

---
+++ exponent.java
@@ -0,0 +1,21 @@
+public class recursion4 {
+    // x^n using recusive method
+    public static double power(int n, double x){
+        //x^n-1
+        //n --> acts as the power
+        //x --> acts as the base
+        if (x==0) {
+            return 0;
+        }
+        else if (n==0) {
+            return 1;
+        }
+        double p = (double)(power(n-1, x));
+        double r = (double)(x*p);
+        return r;
+    }
+    public static void main(String[] args) {
+        double ans = power(3, 2.1);
+        System.out.println(ans);
+    }
+}
